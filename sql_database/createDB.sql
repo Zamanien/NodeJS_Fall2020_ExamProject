8929a1819c53434710adbdf23bf96bc5ab918648
Zamanien <tari0048@stud.kea.dk>
2020-12-13 16:07:41 +0100

    sdasd

--- sql_database/createDB.sql
+++ sql_database/createDB.sql
@@ -1,17 +1,17 @@
 
 -- Create & use database if doesn't exit
 DROP DATABASE IF EXISTS DBUsers;
-CREATE DATABASE DBusers; CHARACTER SET utf8 COLLATE utf8_general_ci;
+CREATE DATABASE DBUsers; CHARACTER SET utf8 COLLATE utf8_general_ci;
 USE DBUsers;
 
 -- Create table inside DB - DBUsers
 CREATE TABLE users (
     id int (10) NOT NULL AUTO_INCREMENT, 
-    usernamme varchar (64) UNIQUE, 
+    username varchar (64) UNIQUE, 
     password CHAR(64),
     email varchar(64),
     PRIMARY KEY (id)
-)
+);
 
 INSERT INTO users (id, username, email, password) VALUES
 (1, 'testUser1', 'test1@gmail.com', '123123123'),
