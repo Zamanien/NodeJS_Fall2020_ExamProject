e291327bba26e3cb50e234f128122998649f62fd
Zamanien <tari0048@stud.kea.dk>
2021-03-31 17:00:49 +0200

    test

--- routes/auth/verify-JWT.js
+++ routes/auth/verify-JWT.js
@@ -5,7 +5,7 @@ module.exports = function (req, res, next) {
   //initiate variable 'token' and pass it value of 'accesstoken' from the cookies passed to browser
   const token = req.cookies.accessToken;
   //if (token == null) return res.status(401).send('ACCESS DENIED - Missing token'); //401 = unauthorized
-  if (token == null) return res.status(401).redirect('http://localhost:8080/login/'); //401 = unauthorized
+  if (token == null) return res.status(401).redirect('/login'); //401 = unauthorized
 
   try {
     const verified = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
@@ -13,6 +13,6 @@ module.exports = function (req, res, next) {
     next();
   } catch (error) {
     //return res.status(403).send('ACCESS DENIED - Invalid token');
-    return res.status(403).redirect('http://localhost:8080/login/');
+    return res.status(403).redirect('/login');
   }
 }
